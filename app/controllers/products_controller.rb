76b8c53fabb56c18d60600c61ab622a70233e766
daschne8 <daschne8@gmail.com>
2019-05-29 11:02:03 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+require 'pry'
+class ProductsController < ApplicationController
+
+  def index
+  end
+
+  def add
+    cart << params[:product]
+    session[:cart] == cart
+    render :index
+  end
+
+end
